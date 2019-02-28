84da7243ffcedad51124b1bc481fb6f021c2f4d6
Dawid Małysa <nnnp.dawid@gmail.com>
2019-02-28 06:57:02 +0100

    MVP

--- mors.py
+++ mors.py
@@ -5,7 +5,71 @@ from itertools import *
 from typing import List, Tuple
 from tree import TreeNode
 
-root = TreeNode()
+root = TreeNode(
+    TreeNode(
+        TreeNode(
+            TreeNode(None, None, 'O'),
+            TreeNode(
+                TreeNode(None, None, "Q"),
+                TreeNode(TreeNode(TreeNode(None, None, ",")), None, "Z"),
+                "G"
+            ),
+            "M"
+        ),
+        TreeNode(
+            TreeNode(
+                TreeNode(None, None, "Y"),
+                TreeNode(
+                    TreeNode(
+                        TreeNode(None, None, "!")
+                    ),
+                    None,
+                    "C"
+                ),
+                "K"
+            ),
+            TreeNode(
+                TreeNode(None, None, "X"),
+                TreeNode(None, None, "B"),
+                "D"
+            ),
+            "N"
+        ),
+        "T"
+    ),
+    TreeNode(
+        TreeNode(
+            TreeNode(
+                TreeNode(None, None, "J"),
+                TreeNode(None, None, "P"),
+                "W"
+            ),
+            TreeNode(
+                TreeNode(None, TreeNode(TreeNode(None, None, "."))),
+                TreeNode(None, None, "L"),
+                "R"
+            ),
+            "A"
+        ),
+        TreeNode(
+            TreeNode(
+                TreeNode(
+                    None,
+                    TreeNode(None, TreeNode(None, None, "?"))
+                ),
+                TreeNode(None, None, "F"),
+                "U"
+            ),
+            TreeNode(
+                TreeNode(None, None, "V"),
+                TreeNode(None, None, "H"),
+                "S"
+            ),
+            "I"
+        ),
+        "E"
+    )
+)
 
 def sin_wave(frec: float = 440.0, fremrate: int = 44100, aplitude: float = 0.5) -> List[float]:
     period = int(fremrate/frec)
@@ -27,9 +91,36 @@ def dumped_wave(frec: float = 440.0, fremrate: int = 44100, aplitude: float = 0.
             for i, s in enumerate(sin_wave(frec, fremrate, aplitude)))
 
 
-def encode(source: str="file.txt", output: str = "file"):
-    with open(source,"r") as source_file:
-        for sign in source_file.read():
-            code = root.search(sign)
+def encode(source: str = "file.txt", output: str = "file"):
+    with open(output, "w+") as output_file:
+        output_file.write("")
+    with open(source, "r") as source_file:
+        for sign in source_file.read().upper():
+            code = root.search_code(sign) if not sign == " " else "._.._"
+            with open(output, 'a+') as output_file:
+                output_file.write(code+'/')
 
-def decode():
+
+def decode(source: str = "file", output: str = "decoded.txt"):
+    with open(output, "w+") as output_file:
+        output_file.write("")
+    with open(source, "r") as source_file:
+        for i in source_file.read().split("\n")[0].split("/"):
+            if i == "":
+                continue
+            tmp = root
+            if i == "._.._":
+                tmp = " "
+            else:
+                for sign in i:
+                    if sign == '.':
+                        tmp = tmp.left
+                    if sign == "_":
+                        tmp = tmp.right
+            with open(output, "a+") as output_file:
+                print(tmp)
+                output_file.write(tmp.data if isinstance(tmp, TreeNode) else tmp)
+
+
+encode()
+decode()
--- tree.py
+++ tree.py
@@ -1,48 +1,54 @@
 class TreeNode:
-    def __init__(self, right= None, data = None, left=None, parent=None):
+    def __init__(self, right= None, left=None,  data = None,parent=None):
         self.right = right
         self.data = data
         self.left = left
+        if left is not None:
+            self.left.addParent(self)
+        if right is not None:
+            self.right.addParent(self)
         self.parent = parent
 
     def hasLeft(self):
         return self.left
 
     def hasBouth(self):
-        return self.right and self.left
+        return self.right is not None and self.left is not None
 
     def hasData(self):
-        return self.data
+        return self.data is not None
 
     def hasRight(self):
         return self.right
 
-    def search(self, data):
+    def search_code(self, data):
         toret = ''
         if self.parent is not None:
-            toret += '.' if self.parent.left.data == self.data else '_'
+            toret += '.' if self.parent.left == self else '_'
         if self.data == data:
             return toret
         else:
-            toret1 = self.left.search(data)
-            toret2 = self.right.search(data)
+            toret1 = self.left.search_code(data) if self.hasLeft() else None
+            toret2 = self.right.search_code(data) if self.hasRight() else None
             if toret1 is not None:
                 toret += toret1
                 return toret
             elif toret2 is not None:
                 toret += toret2
                 return toret
+            else:
+                return None
 
 
     def addParent(self, parent):
-        if not parent.left:
-            parent.left = self
-        elif not parent.right:
-            parent.right = self
-        else:
-            return False
+        # if not parent.left:
+        #     parent.left = self
+        # elif not parent.right:
+        #     parent.right = self
+        # else:
+        #     return False
         self.parent = parent
         return True
 
     def __repr__(self):
-        return "TreeNode({},{},{})".format("Object" if isinstance(self.right, TreeNode) else self.right, self.left, "None" if not isinstance(self.parent, TreeNode) and self.parent is not None else "Obejct")
+        return "TreeNode({},{},{},{})".format("Object" if isinstance(self.right, TreeNode) else self.right, self.left, self.data, "None" if not isinstance(self.parent, TreeNode) and self.parent is not None else "Obejct")
